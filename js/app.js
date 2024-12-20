5d1e4292c43db5bc64f29cc96c1a5f5639578dae
chima1968 <chimarokeumunna98@gmail.com>
2024-12-20 15:42:47 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,23 @@
+const color_box=document.getElementById('color-box');
+
+color_box.addEventListener('click',()=>{
+    color_box.style.backgroundColor=color_box.style.backgroundColor==='red'? 'blue':'red';
+});
+
+
+
+function getrandomcolor(){
+
+    const letters='0123456789ABCDEF';
+    let color='#';
+    for (let i=0;i<6;i++){
+        color+=letters[Math.floor(Math.random()*16)];
+    }
+    return color;
+};
+
+const  change_color=document.getElementById('change-color-btn');
+
+change_color.addEventListener('click',()=>{
+    color_box.style.backgroundColor='black'//getrandomcolor();
+});
